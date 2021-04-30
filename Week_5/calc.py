74297ea46e353e32e12f58def6975d2e7b1efbb7
pattons-OSU <pattons@oregonstate.edu>
2021-04-29 21:37:41 -0700

    pushing final calc and unit test files

--- Week_5/calc.py
+++ Week_5/calc.py
@@ -1,5 +1,25 @@
+#! /usr/bin/env python3
+
+"""
+    Simeon Patton
+    OSU CS362 Spring 2021
+    In class activity 3 - Unit Test
+
+        a) write a simple calc app
+        b) your app should take two numbers as imputs (a,b)
+        c) should have the following methods:
+           addition, subtraction, multiplication, division
+        d) Create a test module that has your test cases
+"""
+
 def add(a,b):
     return a + b
 
 def subtract(a,b):
     return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
--- Week_5/test_calc.py
+++ Week_5/test_calc.py
@@ -1,12 +1,29 @@
+#! /usr/bin/env python3
+
 import unittest
 import  calc
+
+"""
+    Simeon Patton
+    OSU CS362 Spring 2021
+    In class activity 3 - Unit Test
+
+        a) write a simple calc app
+        b) your app should take two numbers as imputs (a,b)
+        c) should have the following methods:
+           addition, subtraction, multiplication, division
+        d) Create a test module that has your test cases
+
+        Each test case method has one test that will pass and one that will fail
+        This allows us to visualize the difference in the terminal output.
+"""
 
 class testCaseAdd(unittest.TestCase):
     ## this test should pass
     def test_add(self):
         self.assertEqual(calc.add(10,5), 15)
 
-## this test will fail
+    ## this test will fail
     def test_add_2(self):
         self.assertEqual(calc.add(1,5), 8)
 
@@ -15,9 +32,27 @@ class testCaseSubtract(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(calc.subtract(10,5), 5)
 
-## This test will fail
+    ## This test will fail
     def test_subtract_2(self):
         self.assertEqual(calc.add(1,5), 8)
+
+class testCaseMultiply(unittest.TestCase):
+    ## This test should pass
+    def test_multiplication(self):
+        self.assertEqual(calc.multiply(5,5), 25)
+
+    ## this test should fail
+    def test_multiplication_2(self):
+        self.assertEqual(calc.multiply(5,5), 50)
+
+class testCaseDivide(unittest.TestCase):
+    ## This test should pass
+    def test_divide(self):
+        self.assertEqual(calc.divide(25,5), 5)
+
+    ## This test should fail
+    def test_divide_2(self):
+        self.assertEqual(calc.divide(25,5), 2)
 
         
 
