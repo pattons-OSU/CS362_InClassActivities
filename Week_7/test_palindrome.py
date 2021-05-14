277238a81bc2e1ebd0ec769ad95beafb3657eb5e
pattons-OSU <pattons@oregonstate.edu>
2021-05-14 15:13:01 -0700

    creating palindrome unit test program

---
+++ Week_7/test_palindrome.py
@@ -0,0 +1,24 @@
+#! python3
+
+"""
+Simeon Patton
+Inclass activity Week 7
+CS362 - OSU Spring 2021
+
+Palindrome -    1. Ask the user for a string and determine whether it is a palindrome or not. 
+                2. Write tests for the above specification using unittest and pytest.
+                3. Document the outputs in a pdf - (screenshots of the output from unittest and pytest)
+
+    Some code inspiration from www.shorturl.at/eglJ6
+    
+
+"""
+
+import unittest
+import palindrome
+
+class testCaseVolume(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
