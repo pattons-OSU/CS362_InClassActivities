ea2f9df41d9f4f915c1754951bdc3136477dacad
pattons-OSU <pattons@oregonstate.edu>
2021-05-14 14:50:38 -0700

    creating word count program

---
+++ Week_7/word_count.py
@@ -0,0 +1,17 @@
+#! python3
+
+"""
+Simeon Patton
+Inclass activity Week 7
+CS362 - OSU Spring 2021
+
+Word Count-
+        1. Ask the user for a sentence and determine the number of
+        words in that sentence.
+        Example - “This is an activity”, Output - 4.
+        2. Write tests for the above specification using unittest and
+        pytest.
+        3. Document the outputs in a pdf - (screenshots of the output
+        from unittest and pytest)    
+
+"""
