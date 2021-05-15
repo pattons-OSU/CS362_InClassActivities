467b0159c74fa37e1bfb5535b2f8b00c1ae3746b
pattons-OSU <pattons@oregonstate.edu>
2021-05-15 10:43:34 -0700

    working pytest word count

--- Week_7/pytest_word_count.py
+++ Week_7/pytest_word_count.py
@@ -13,3 +13,21 @@ Word Count-
         3. Document the outputs in a pdf - (screenshots of the output
         from unittest and pytest)    
 """
+import word_count
+import pytest
+
+## I was forced to hardcode the input on this one too as the pytest
+## module would not allow the user to input into the program eventhough
+## it was called as seen below
+@pytest.fixture
+def user_input():
+    ##user_input = word_count.user_input()
+    user_input = "Hello I had to hardcode this too"
+    return user_input
+
+## Pytest, testing to see if user input is a string and not another data type
+def test_type(user_input):
+    assert type(user_input) is str
+
+if __name__ == '__main__':
+    test_type(user_input)
