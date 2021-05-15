aa4323b054d81d55f752cdb4936a285ffdf048b4
pattons-OSU <pattons@oregonstate.edu>
2021-05-15 10:35:39 -0700

    updating test_word_count

--- Week_7/test_word_count.py
+++ Week_7/test_word_count.py
@@ -1,5 +1,4 @@
 #! python3
-
 """
 Simeon Patton
 Inclass activity Week 7
@@ -13,14 +12,20 @@ Word Count-
         pytest.
         3. Document the outputs in a pdf - (screenshots of the output
         from unittest and pytest)    
-
 """
-
 import unittest
 import word_count
 
+
 class testCaseVolume(unittest.TestCase):
-    pass
+##Unit Test  
+##Testing pass and fail of the input type
+    ## Testing to see if the input is a type value of "string"
+    def test_type(self):
+      self.assertTrue(type(word_count.user_input) is str)
+    ## This test should fail as the input type IS string
+    def test_type_fail(self):
+      self.assertTrue(type(word_count.user_input) is not str)
 
 if __name__ == '__main__':
     unittest.main()
