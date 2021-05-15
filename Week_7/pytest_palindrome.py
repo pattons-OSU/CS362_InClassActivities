75d19483799fdbb3050df09d61075583a02eddf2
pattons-OSU <pattons@oregonstate.edu>
2021-05-15 10:27:01 -0700

    updating pytst code

--- Week_7/pytest_palindrome.py
+++ Week_7/pytest_palindrome.py
@@ -8,7 +8,6 @@ Palindrome -    1. Ask the user for a string and determine whether it is a palin
                 2. Write tests for the above specification using unittest and pytest.
                 3. Document the outputs in a pdf - (screenshots of the output from unittest and pytest)
 
-
     Some code inspiration from www.shorturl.at/eglJ6
     
 """
@@ -18,7 +17,6 @@ import pytest
 ## Had to hard code a user input here because the "fixture"
 ## was not found otherwise. Hardcode suggested for final draft
 ## by TA Raghuram
-
 @pytest.fixture
 def user_input():
     return "hello world"
