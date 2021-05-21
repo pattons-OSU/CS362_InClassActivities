8f34f05a450ad0855fa4b1f7fc241c6368354efc
pattons-OSU <pattons@oregonstate.edu>
2021-05-20 21:40:28 -0700

    test file "test_leap_year" written and committed

---
+++ Week_8/test_leap_year.py
@@ -0,0 +1,26 @@
+#! python3
+
+"""
+Simeon Patton
+CS362 Oregon State - Spring 2021
+In class activity week 8
+
+Apply the following steps to the leap year code in Homework - 1
+    1.Write tests for leap year program before you begin implementation.
+    2.Make a commit.
+    3.Write code and then run the test - then make a commit.
+    4.Document Output with unittest.
+
+"""
+
+import unittest
+import leap_year
+
+class testCaseVolume(unittest.TestCase):
+    ## this test should pass, testing the actual arithmetic of the function
+    def test_data_type(self):
+        self.assertTrue(type(in_year) is int)
+
+
+if __name__ == '__main__':
+    unittest.main()
