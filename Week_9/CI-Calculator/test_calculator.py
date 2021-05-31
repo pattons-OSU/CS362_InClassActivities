20db36509548b8bd5dd568f463396372c70bed23
pattons-OSU <pattons@oregonstate.edu>
2021-05-31 13:29:39 -0700

    after linting test_calculator

--- Week_9/CI-Calculator/test_calculator.py
+++ Week_9/CI-Calculator/test_calculator.py
@@ -9,11 +9,11 @@ OSU CS362 Spring 2021
 """
 
 import calculator
-import pytest
+
 
 class TestCalculatorApp:
     def test_add(self):
-        assert 5 == calculator.add(3,2)
-    
+        assert 5 == calculator.add(3, 2)
+
     def test_subtract(self):
-        assert 3 == calculator.subtract(96,3)
+        assert 3 == calculator.subtract(96, 3)
