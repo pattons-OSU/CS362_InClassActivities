b6abd5f8d246d64630661f59727a17da2f53e2be
pattons-OSU <pattons@oregonstate.edu>
2021-05-31 12:59:33 -0700

    created CI-Calculator & venv calculatorApp folder

---
+++ Week_9/CI-Calculator/calculator.py
@@ -0,0 +1,16 @@
+#! Python 3
+
+"""
+Simeon Patton
+Calculator App using circle CI
+Inclass Activity week 9
+OSU CS362 Spring 2021
+
+"""
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
